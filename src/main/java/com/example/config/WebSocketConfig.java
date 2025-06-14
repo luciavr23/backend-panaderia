68e3dcabc770e8a0afbbe978e322d65895cc4329
Lucia Villar Rodriguez <147031304+luciavr23@users.noreply.github.com>
2025-06-14 19:46:20 +0200

    Update WebSocketConfig.java

--- src/main/java/com/example/config/WebSocketConfig.java
+++ src/main/java/com/example/config/WebSocketConfig.java
@@ -12,7 +12,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+		registry.addEndpoint("/ws")
+        .setAllowedOrigins("http://localhost:3000", "https://frontend-panaderia.vercel.app")
+        .withSockJS();
+
 	}
 
 	@Override
